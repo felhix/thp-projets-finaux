ed07f40f570069c8fcb4af5ee47257345dab0361
david-dacruz <dvddcrz@gmail.com>
2018-02-16 18:52:11 +0100

    Status button show page and index done as well as project edit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,26 @@ module ApplicationHelper
     return markdown.render(text).html_safe
   end
 
+  def gravatar_for(user, options = { size: 80})
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.email, class: "rounded-circle")
+  end
+
+  def is_approved?(project)
+    @projects = Project.all
+    @projects.each do |project|
+      if project.approved
+        return '<span class="status badge badge-success float-right" id="status">
+                        <strong>Validé</strong>
+                      </span>'.html_safe
+      else
+         return '<span class="status badge badge-warning float-right" id="status">
+                       <strong>Pas encore validé</strong>
+                      </span>'.html_safe
+      end  
+    end 
+  end 
+
 end
