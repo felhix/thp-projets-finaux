e6bf6e67437e22788db301a05f7555dfaf8b63fb
david-dacruz <dvddcrz@gmail.com>
2018-02-17 14:25:29 +0100

    partials created for project index

---
+++ app/views/pages/partials/_welcome.html.rb
@@ -0,0 +1,5 @@
+<div class="card card-body bg-light" style="margin-top: 100px;">
+ <h3 class="text-center">The Final Projects</h3>
+ <hr>
+	<%= link_to "Hack in", new_user_session_path, class: "btn btn-primary btn-block"  %>
+</div>
