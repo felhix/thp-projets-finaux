65f6f5648d27d07d51ca7f3bcc3d34e322212266
david-dacruz <dvddcrz@gmail.com>
2018-02-17 19:29:40 +0100

    bg img fix for heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,11 +39,11 @@ module ApplicationHelper
     @projects = Project.all
     @projects.each do |project|
       if project.approved
-        return '<span class="badge badge-pill badge-success home-pill">
+        return '<span class="badge badge-pill badge-success" id="home-pill">
                         <strong>Validé</strong>
                       </span>'.html_safe
       else
-         return '<span class="badge badge-pill badge-warning home-pill">
+         return '<span class="badge badge-pill badge-warning" id="home-pill">
                        <strong>Pas encore validé</strong>
                       </span>'.html_safe
       end  
