c76379f18252c1e7e427b2135647a573341b2840
david-dacruz <dvddcrz@gmail.com>
2018-02-17 16:38:41 +0100

    background image for homa page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,130 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-10.times do 
-
 	Project.create([
-			{ title: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'},
-			{ short_description: '' },
-			{ description: '' },
-			{ pitch: '' },
-			{ user_id: '' },
-			{ created_at: '' },
-			{ updated_at: '' },
-			])
+			{ title: 'Lorem ipsum dolor sit amet, consectetur cras amet.'},
+			{ short_description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...' },
+			{ description: '
+						# Sine dedisset viso eadem contendere nurusque
 
-end
+						## Petitur gravitate
+
+						**Lorem markdownum texerat** fixis funus Ecce induitur, ipse, et nec onus!
+						Adnuerat ducit est iuventa *sunt vulnera*, et inde nata murmure Clytien tollit.
+						In est haurit, modo nox, miserarum scopulos poscat,
+						[hunc](http://sacradocuique.net/ades) quam recisum, ama.
+
+						## Nunc Neptune oris castra Eumenides dedistis curvataque
+
+						Una datus potentia agitque adspexit media ergo vertunt reformatus Inachus et
+						ingentia poscit habetque dicor, nec mutatas **vastat**. Vox medio possim,
+						defendere Romanae: non quam utrumque, superest supplex fusus
+						[ad](http://et.net/) iam, nox haut. Terrae habentem aliquis vagatur posse
+						nebulas adamante ubi vincula victus: sanguis petit. Sinat alta arbore adiuro per
+						canistris citharae summos. Suis **tardi accendit** presserat pubis ne venit et
+						imbre minor Italicis undis, quo.
+
+						- Aetatis ramis in pondus tuos aquae
+						- Respiciens quas
+						- Gratissima rami incubuit fidem
+
+						## Parta hinc esset et scopulis dominatur servitura
+
+						Aut exempla At vacat cornua miscet reserabo classe ara aut quosque vulgata.
+						Interque porreximus misit, habent exul circumsonat occupat si erat liquefactis
+						sume! Trahuntur versat. Hunc tua profanos, eminus sanguine aura sentiat remissis
+						in auratum imber. Sed dedi licet ruent iustitia tamque fragor Hyrie, misit
+						caput.
+
+						## Falle decore fisso profundo
+
+						Tristia matris nutu [causa vereri nymphae](http://www.iamet.net/). Bovis nega
+						comitum *caelo ense* temptant germana liquebat pariter; est primi acerris
+						vitiaverat erant, prohibentque damno vestigia. Pectore agentis myrteta tetigere
+						fervebant tinxit accipe: nocet sola? Ille multicavo tu arbor gravidus genitor
+						[placet ullus flava](http://vero.io/) at ullis Proximus quae, notavit tinxit.
+
+						    maximizeLanguageDomain = voipTunnelingMount / 628381 +
+						            keystroke_webmaster_vaporware - sequence;
+						    dragArtificialArt = donationware;
+						    if (tiger(dslamYottabyte(smartphone_num_webmail), signature_cd,
+						            upGrayscale)) {
+						        ibm += meta(4, programVleReal, udp);
+						        tooltipClient = address_iscsi_ups.seo_pop_runtime(68, component);
+						        mount_boot -= infotainment_boot;
+						    } else {
+						        permalink = card_multithreading_word;
+						        mamp_ftp(repeaterMatrix / handleCopyToken, 5, url);
+						    }
+
+						Non dare miratur dare iusta densus et turpe, numina inspirat nimius Pelasgi
+						quandoquidem, inaequalis quique. Ieiunia dixit telum amanti videtur ambae
+						Tritonida, inde fortia, eadem. Deseruitque quorum: olim fallere, ardentem
+						foribusque parte quatenus. [Licha](http://fertur.net/ignis.php) specularer suam,
+						ferre stabat Mnemonides, eritque ripam, hausto astra, at tamen aliqua haerebat
+						efflant comis.
+
+				' },
+			{ pitch: '
+					# Sine dedisset viso eadem contendere nurusque
+
+					## Petitur gravitate
+
+					**Lorem markdownum texerat** fixis funus Ecce induitur, ipse, et nec onus!
+					Adnuerat ducit est iuventa *sunt vulnera*, et inde nata murmure Clytien tollit.
+					In est haurit, modo nox, miserarum scopulos poscat,
+					[hunc](http://sacradocuique.net/ades) quam recisum, ama.
+
+					## Nunc Neptune oris castra Eumenides dedistis curvataque
+
+					Una datus potentia agitque adspexit media ergo vertunt reformatus Inachus et
+					ingentia poscit habetque dicor, nec mutatas **vastat**. Vox medio possim,
+					defendere Romanae: non quam utrumque, superest supplex fusus
+					[ad](http://et.net/) iam, nox haut. Terrae habentem aliquis vagatur posse
+					nebulas adamante ubi vincula victus: sanguis petit. Sinat alta arbore adiuro per
+					canistris citharae summos. Suis **tardi accendit** presserat pubis ne venit et
+					imbre minor Italicis undis, quo.
+
+					- Aetatis ramis in pondus tuos aquae
+					- Respiciens quas
+					- Gratissima rami incubuit fidem
+
+					## Parta hinc esset et scopulis dominatur servitura
+
+					Aut exempla At vacat cornua miscet reserabo classe ara aut quosque vulgata.
+					Interque porreximus misit, habent exul circumsonat occupat si erat liquefactis
+					sume! Trahuntur versat. Hunc tua profanos, eminus sanguine aura sentiat remissis
+					in auratum imber. Sed dedi licet ruent iustitia tamque fragor Hyrie, misit
+					caput.
+
+					## Falle decore fisso profundo
+
+					Tristia matris nutu [causa vereri nymphae](http://www.iamet.net/). Bovis nega
+					comitum *caelo ense* temptant germana liquebat pariter; est primi acerris
+					vitiaverat erant, prohibentque damno vestigia. Pectore agentis myrteta tetigere
+					fervebant tinxit accipe: nocet sola? Ille multicavo tu arbor gravidus genitor
+					[placet ullus flava](http://vero.io/) at ullis Proximus quae, notavit tinxit.
+
+					    maximizeLanguageDomain = voipTunnelingMount / 628381 +
+					            keystroke_webmaster_vaporware - sequence;
+					    dragArtificialArt = donationware;
+					    if (tiger(dslamYottabyte(smartphone_num_webmail), signature_cd,
+					            upGrayscale)) {
+					        ibm += meta(4, programVleReal, udp);
+					        tooltipClient = address_iscsi_ups.seo_pop_runtime(68, component);
+					        mount_boot -= infotainment_boot;
+					    } else {
+					        permalink = card_multithreading_word;
+					        mamp_ftp(repeaterMatrix / handleCopyToken, 5, url);
+					    }
+
+					Non dare miratur dare iusta densus et turpe, numina inspirat nimius Pelasgi
+					quandoquidem, inaequalis quique. Ieiunia dixit telum amanti videtur ambae
+					Tritonida, inde fortia, eadem. Deseruitque quorum: olim fallere, ardentem
+					foribusque parte quatenus. [Licha](http://fertur.net/ignis.php) specularer suam,
+					ferre stabat Mnemonides, eritque ripam, hausto astra, at tamen aliqua haerebat
+					efflant comis.
+
+					' },
+			{ user_id: '1' }
+			])
