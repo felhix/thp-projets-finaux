8f78b2c7193e2d2ad259aaf420cee6222b7461ee
Showner <rnshow78@gmail.com>
2018-02-18 22:49:36 +0100

    add seed testuser

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create(email: "t@t.t", password: "foobar", password_confirmation: "foobar", first_name: "foo", last_name: "bar")
 
 1.times do
 	Project.create([
